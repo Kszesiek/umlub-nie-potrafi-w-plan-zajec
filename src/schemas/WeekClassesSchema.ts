bca17cd23998b7f88e3c53752de48a66148d1074
Kszesiek <kszesiek@gmail.com>
2024-10-25 23:44:05 +0200

    Got rid of deprecated files

--- src/schemas/WeekClassesSchema.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-import {singleClassSchema} from "./SingleClassSchema";
-
-export const weekClassesSchema = z.object({
-  week: z.number(),
-  classes: z.array(singleClassSchema),
-})
-
-export type WeekClasses = z.infer<typeof weekClassesSchema>;
